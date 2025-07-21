c9e204bd302015793389e4fc07fad483eae3f017
정은선 <jeong7653@naver.com>
2025-07-22 08:25:20 +0900

    init: firts commit

---
+++ EunsunJeong/backend/mypage/src/main/java/com/backend/mypage/MypageApplication.java
@@ -0,0 +1,13 @@
+package com.backend.mypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypageApplication.class, args);
+    }
+
+}
---
+++ EunsunJeong/backend/mypage/src/test/java/com/backend/mypage/MypageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
